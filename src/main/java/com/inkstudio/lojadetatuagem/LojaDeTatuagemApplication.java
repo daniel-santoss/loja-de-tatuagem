7637403464472c800d959459836c656c05fba286
Daniel Vinicius <danielvinicius.santos7@gmail.com>
2023-08-26 14:44:32 -0300

    Criação do ambiente de desenvolvimento

---
+++ src/main/java/com/inkstudio/lojadetatuagem/LojaDeTatuagemApplication.java
@@ -0,0 +1,13 @@
+package com.inkstudio.lojadetatuagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaDeTatuagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaDeTatuagemApplication.class, args);
+	}
+
+}
